20876a685077e9adbb80b5f94b4ab236cc109160
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-06-26 22:56:51 +0200

    basic jest ts setup

---
+++ reverse-linked-list/main.test.ts
@@ -0,0 +1,8 @@
+import func from './main'
+
+it('returns 1', () => {
+  const res = func()
+  expect(res).toBe(1)
+})
+
+export {}
---
+++ reverse-linked-list/main.ts
@@ -0,0 +1,5 @@
+const reverse = () => {
+  return 1
+}
+
+export default reverse
